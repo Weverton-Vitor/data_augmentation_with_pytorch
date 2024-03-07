0d74bc11762c575c51f91b517d4201f969174153
Weverton Vitor <wevertonvitor01@gmail.com>
2024-03-06 21:45:35 -0300

    Create split data function

--- lib.py
+++ lib.py
@@ -1,12 +1,12 @@
 import requests
 import py7zr
 import os
+import shutil
+
 from CustomLandfillDataset import CustomLandfillDataset
 from sklearn.model_selection import train_test_split
 from PIL import Image
 import pandas as pd
-import numpy as np
-import os
 
 
 def get_data(url: str, file_name: str):
@@ -17,17 +17,20 @@ def get_data(url: str, file_name: str):
         # Get file in dropbox
         response = requests.get(url=url)
 
-        print("Downloading files...")
+        print("-> Downloading files...")
         if response.ok:
             with open(file_name, 'wb') as file: # Save file
                 file.write(response.content)
-            print("File downloaded successfully.")
+            print("-> File downloaded successfully.")
         else:
-            print(f"Error: response isn't ok : {response.status_code}")
+            print(f"-> Error: response isn't ok : {response.status_code}")
+    
+    else:
+        print(f"-> File {file_name} is already downloaded, to donwload again remove it manually")
 
     # Extract files
     if files_path not in os.listdir():
-        print("Extracting files...")
+        print("-> Extracting files...")
         with py7zr.SevenZipFile(file_name, mode='r') as z:
             z.extractall('./')
 
@@ -39,32 +42,80 @@ def get_data(url: str, file_name: str):
 
         print("Files extracted successfully.")
 
-
-def create_masks(train_path: str, train_labels: str, annotations_path):
-    dataFrame = pd.read_csv(train_labels, usecols=["Idx", "Image Index", "IsLandfill"])
-    label = pd.read_csv(train_labels, usecols=["IsLandfill"])
-
-    train_dataset = CustomLandfillDataset(data=dataFrame.values.tolist(),
-                                        dsType = 'train',
-                                        transforms=None,
-                                        labelCSV=train_labels,
-                                        imgpath=train_path,
-                                        jsonpath=annotations_path)
-
-    if 'masks' not in os.listdir(train_path):
-        print(f"Create folder masks in {f'{train_path}/masks'}...")
-        os.mkdir(f'{train_path}/masks')
-
-        print("Create masks...")
-        for img in train_dataset:
-            mask = Image.fromarray(img['binary_mask'].numpy(), "L")
-            mask.save(f'{train_path}/masks/{img["name"].split(".")[0]}_mask.png')
-
-        print('Masks created successfully')
-
     else:
-        print(f"'mask' folder already exist inside {train_path} folder")
+        print(f"-> File {file_name} is already extracted, to extract again remove it and its subfolders manually")
 
 
-def split_data(train_rate: float, train_labels: str) -> None:
-    dataFrame = pd.read_csv(train_labels, usecols=["Idx", "Image Index", "IsLandfill"])
+def create_masks(data_path: str, labels_path: str, annotations_path):
+    files = [f for f in os.listdir(data_path) if os.path.isfile(f'{data_path}/{f}')]
+    if not files:
+        print('-> Data path empty or already preprocessed')
+    else:
+        dataFrame = pd.read_csv(labels_path, usecols=["Idx", "Image Index", "IsLandfill"])
+        train_dataset = CustomLandfillDataset(data=dataFrame.values.tolist(),
+                                            dsType = 'train',
+                                            transforms=None,
+                                            labelCSV=labels_path,
+                                            imgpath=data_path,
+                                            jsonpath=annotations_path)
+
+        if 'masks_temp' not in os.listdir(data_path):
+            print(f"-> Create folder masks_temp in {f'{data_path}/masks_temp'}...")
+            os.mkdir(f'{data_path}/masks_temp')
+
+            print("Create masks...")
+            for img in train_dataset:
+                mask = Image.fromarray(img['binary_mask'].numpy(), "L")
+                mask.save(f'{data_path}/masks_temp/{img["name"].split(".")[0]}_mask.png')
+
+            print('-> Masks created successfully')
+
+        else:
+            print(f"-> 'masks_temp' folder already exist inside {data_path} folder")
+
+
+def split_data(data_path: str, labels_path: str, test_ratio: float,) ->  None:
+    df = pd.read_csv(labels_path, usecols=["Idx", "Image Index", "IsLandfill"])
+    label = pd.read_csv(labels_path, usecols=["IsLandfill"])
+
+    train_df, test_df, train_lab, test_lab = train_test_split(df,
+                                                                label,
+                                                                test_size = test_ratio,
+                                                                shuffle=True, 
+                                                                stratify=label)
+    
+    # if 'train' not in os.listdir(data_path):
+    #     print(f"-> Create folder train in {f'{data_path}/train'}...")
+    #     print(f"-> Create folder masks in {f'{data_path}/train/masks'}...")
+
+    #     os.mkdir(f'{data_path}/train')
+    #     os.mkdir(f'{data_path}/train/masks')
+        
+    #     print("Move images and masks to train folder")
+    #     for image in train_df['Image Index']:
+    #         shutil.move(f'{data_path}/{image}', f'{data_path}/train/{image}')
+    #         shutil.move(f'{data_path}/masks_temp/{image.split(".")[0]}_mask.png', f'{data_path}/train/masks/{image.split(".")[0]}_mask.png')
+
+    # Create folder train and move the selected data
+    def split(path: str, df: pd.DataFrame) ->  None:
+        if path not in os.listdir(data_path):
+            print(f"-> Create folder train in {f'{data_path}/train'}...")
+            print(f"-> Create folder masks in {f'{data_path}/train/masks'}...")
+
+            os.mkdir(f'{data_path}/{path}')
+            os.mkdir(f'{data_path}/{path}/masks')
+            
+            print(f"-> Move images and masks to {path} folder")
+            for image in df['Image Index']:
+                shutil.move(f'{data_path}/{image}', f'{data_path}/{path}/{image}')
+                shutil.move(f'{data_path}/masks_temp/{image.split(".")[0]}_mask.png', f'{data_path}/{path}/masks/{image.split(".")[0]}_mask.png')
+
+    if 'train' not in os.listdir(data_path):
+        split(path='train', df=train_df)
+    
+    if 'test' not in os.listdir(data_path):
+        split(path='test', df=test_df)
+    
+    print(f"-> Deleting {data_path}/masks_temp/'")
+    if 'masks_temp' in os.listdir(data_path):
+        shutil.rmtree(f'{data_path}/masks_temp/')
--- main.py
+++ main.py
@@ -2,15 +2,17 @@ from lib import get_data, create_masks, split_data
 
 ULR_FILE = 'https://www.dropbox.com/s/8huwne6of80v5l3/Pansharpened.7z?dl=1'
 FILE_NAME = 'Pansharpened.7z'
-TRAIN_PATH = './HR_TIF_Files'
-TRAIN_LABELS = './Pansharpened/PanSharpenedData.csv'
+DATA_PATH = './HR_TIF_Files'
+LABELS_PATH = './Pansharpened/PanSharpenedData.csv'
 PANSHARPENED_JSON = './LandfillCoordPolygons'
 TEST_RATIO = 0.2
 
-
-
 if __name__ == '__main__':
     get_data(url=ULR_FILE, file_name=FILE_NAME)
-    create_masks(train_path=TRAIN_PATH,
-                 train_labels=TRAIN_LABELS,
+    create_masks(data_path=DATA_PATH,
+                 labels_path=LABELS_PATH,
                  annotations_path=PANSHARPENED_JSON)
+
+    split_data(data_path=DATA_PATH,
+               labels_path=LABELS_PATH,
+               test_ratio= TEST_RATIO)
