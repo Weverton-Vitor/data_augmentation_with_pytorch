3b32a8825411f50bd88274dc76a71a442b95fffb
Weverton Vitor <wevertonvitor01@gmail.com>
2024-03-07 13:22:19 -0300

    Fix

--- data_augmentation.py
+++ data_augmentation.py
@@ -18,11 +18,16 @@ def apply_data_agumentation_to_train(train_path):
         mask_name = mask.split('.')[1].split('/')[-1].replace('_mask', '')
         mask = Image.open(mask)
 
-        image = TF.to_tensor(image.astype(np.float32))
+        # print('----------------------------------------')
+        # print(image.min(), image.max())
+        image = TF.to_tensor(image.astype(np.int64))
+        # print(image.numpy().min(), image.numpy().max())
         
         #random horizontal flip
         image_h_flip = TF.hflip(image)
         mask_h_flip = TF.hflip(mask)
+        # print(image_h_flip.numpy().min(), image_h_flip.numpy().max())
+
         with tifffile.TiffWriter(f"{train_path}/{image_name}_h_flip.tif") as tif:
             tif.write(image_h_flip.numpy(), shape=image.shape)
         mask_h_flip.save(f"{train_path}/masks/{mask_name}_h_flip_mask.png")
@@ -30,17 +35,21 @@ def apply_data_agumentation_to_train(train_path):
         #random vertical flip
         image_v_flip = TF.vflip(image)
         mask_v_flip = TF.vflip(mask)
+        # print(image_v_flip.numpy().min(), image_v_flip.numpy().max())
         with tifffile.TiffWriter(f"{train_path}/{image_name}_v_flip.tif") as tif:
             tif.write(image_v_flip.numpy(), shape=image.shape)
         mask_v_flip.save(f"{train_path}/masks/{mask_name}_v_flip_mask.png")
 
 
         # # random rotation
-        image_rotated = TF.rotate(image, angle=10.0)
-        mask_rotated = TF.rotate(mask, angle=10.0)
+        image_rotated = TF.rotate(image, angle=25.0)
+        mask_rotated = TF.rotate(mask, angle=25.0)
+        # print(image_rotated.numpy().min(), image_rotated.numpy().max())
+
         with tifffile.TiffWriter(f"{train_path}/{image_name}_rotated.tif") as tif:
             tif.write(image_rotated.numpy(), shape=image.shape)
         mask_rotated.save(f"{train_path}/masks/{mask_name}_rotated_mask.png")
 
+        # print('----------------------------------------')
 
     print("-> Finish data augmentation")
