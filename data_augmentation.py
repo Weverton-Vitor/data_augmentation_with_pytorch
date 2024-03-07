f1dcc54327799615e758543a98291f26618556f9
Weverton Vitor <wevertonvitor01@gmail.com>
2024-03-07 06:04:24 -0300

    Create data augmentation function

---
+++ data_augmentation.py
@@ -0,0 +1,46 @@
+import os
+import tifffile
+from PIL import Image
+import numpy as np
+from torchvision.transforms import functional as TF
+
+
+def apply_data_agumentation_to_train(train_path):
+    images = [f'{train_path}/{img}' for img in os.listdir(train_path) if os.path.isfile(f'{train_path}/{img}')]
+    masks = [f'{train_path}/masks/{mask}' for mask in os.listdir(train_path+'/masks') if os.path.isfile(f'{train_path}/masks/{mask}')]
+
+    print("-> Applying vertical flip, horizontal flip and rotate separately")
+    for img_path in images:
+        image = tifffile.imread(img_path)
+        image_name = img_path.split('/')[-1].split('.')[0]
+
+        mask = list(filter(lambda x: image_name in x, masks))[0]
+        mask_name = mask.split('.')[1].split('/')[-1]
+        mask = Image.open(mask)
+
+        image = TF.to_tensor(image.astype(np.float32))
+        
+        #random horizontal flip
+        image_h_flip = TF.hflip(image)
+        mask_h_flip = TF.hflip(mask)
+        with tifffile.TiffWriter(f"{train_path}/{image_name}_h_flip.tif") as tif:
+            tif.write(image_h_flip.numpy())
+        mask_h_flip.save(f"{train_path}/masks/{mask_name}_h_flip.png")
+
+        #random vertical flip
+        image_v_flip = TF.vflip(image)
+        mask_v_flip = TF.vflip(mask)
+        with tifffile.TiffWriter(f"{train_path}/{image_name}_v_flip.tif.png") as tif:
+            tif.write(image_v_flip.numpy())
+        mask_v_flip.save(f"{train_path}/masks/{mask_name}_v_flip.png")
+
+
+        # # random rotation
+        image_rotated = TF.rotate(image, angle=10.0)
+        mask_rotated = TF.rotate(mask, angle=10.0)
+        with tifffile.TiffWriter(f"{train_path}/{image_name}_rotated.tif") as tif:
+            tif.write(image_rotated.numpy())
+        mask_rotated.save(f"{train_path}/masks/{mask_name}mask_rotated.png")
+
+
+    print("-> Finish data augmentation")
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from data_augmentation import apply_data_agumentation_to_train
 from lib import get_data, create_masks, split_data
 
 ULR_FILE = 'https://www.dropbox.com/s/8huwne6of80v5l3/Pansharpened.7z?dl=1'
 FILE_NAME = 'Pansharpened.7z'
 DATA_PATH = './HR_TIF_Files'
+TRAIN_PATH = DATA_PATH + '/train'
 LABELS_PATH = './Pansharpened/PanSharpenedData.csv'
 PANSHARPENED_JSON = './LandfillCoordPolygons'
 TEST_RATIO = 0.2
@@ -16,3 +18,5 @@ if __name__ == '__main__':
     split_data(data_path=DATA_PATH,
                labels_path=LABELS_PATH,
                test_ratio= TEST_RATIO)
+    
+    apply_data_agumentation_to_train(train_path=TRAIN_PATH)
