fbd1bfe87abc7a60bf7e7bd0758c5c65a79ee616
Weverton <wevertonvitor01@gmail.com>
2024-03-07 09:24:10 -0300

    Fix

--- data_augmentation.py
+++ data_augmentation.py
@@ -15,7 +15,7 @@ def apply_data_agumentation_to_train(train_path):
         image_name = img_path.split('/')[-1].split('.')[0]
 
         mask = list(filter(lambda x: image_name in x, masks))[0]
-        mask_name = mask.split('.')[1].split('/')[-1]
+        mask_name = mask.split('.')[1].split('/')[-1].replace('_mask', '')
         mask = Image.open(mask)
 
         image = TF.to_tensor(image.astype(np.float32))
@@ -25,14 +25,14 @@ def apply_data_agumentation_to_train(train_path):
         mask_h_flip = TF.hflip(mask)
         with tifffile.TiffWriter(f"{train_path}/{image_name}_h_flip.tif") as tif:
             tif.write(image_h_flip.numpy())
-        mask_h_flip.save(f"{train_path}/masks/{mask_name}_h_flip.png")
+        mask_h_flip.save(f"{train_path}/masks/{mask_name}_h_flip_mask.png")
 
         #random vertical flip
         image_v_flip = TF.vflip(image)
         mask_v_flip = TF.vflip(mask)
-        with tifffile.TiffWriter(f"{train_path}/{image_name}_v_flip.tif.png") as tif:
+        with tifffile.TiffWriter(f"{train_path}/{image_name}_v_flip.tif") as tif:
             tif.write(image_v_flip.numpy())
-        mask_v_flip.save(f"{train_path}/masks/{mask_name}_v_flip.png")
+        mask_v_flip.save(f"{train_path}/masks/{mask_name}_v_flip_mask.png")
 
 
         # # random rotation
@@ -40,7 +40,7 @@ def apply_data_agumentation_to_train(train_path):
         mask_rotated = TF.rotate(mask, angle=10.0)
         with tifffile.TiffWriter(f"{train_path}/{image_name}_rotated.tif") as tif:
             tif.write(image_rotated.numpy())
-        mask_rotated.save(f"{train_path}/masks/{mask_name}mask_rotated.png")
+        mask_rotated.save(f"{train_path}/masks/{mask_name}_rotated_mask.png")
 
 
     print("-> Finish data augmentation")
