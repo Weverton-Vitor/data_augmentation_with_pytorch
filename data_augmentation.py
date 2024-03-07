9584c2758cfeb4b54680c63312e25e2599712446
Weverton Vitor <wevertonvitor01@gmail.com>
2024-03-07 14:19:46 -0300

    Fix

--- data_augmentation.py
+++ data_augmentation.py
@@ -3,6 +3,7 @@ import tifffile
 from PIL import Image
 import numpy as np
 from torchvision.transforms import functional as TF
+from skimage.transform import rotate
 
 
 def apply_data_agumentation_to_train(train_path):
@@ -19,35 +20,38 @@ def apply_data_agumentation_to_train(train_path):
         mask = Image.open(mask)
 
         # print('----------------------------------------')
-        # print(image.min(), image.max())
+        # print(image.min(), image.max(), image.dtype)
         image = TF.to_tensor(image.astype(np.int64))
-        # print(image.numpy().min(), image.numpy().max())
+        # print(image.numpy().min(), image.numpy().max(), image.dtype)
         
         #random horizontal flip
         image_h_flip = TF.hflip(image)
         mask_h_flip = TF.hflip(mask)
-        # print(image_h_flip.numpy().min(), image_h_flip.numpy().max())
+        # print(image_h_flip.numpy().min(), image_h_flip.numpy().max(), image_h_flip.numpy().dtype)
 
         with tifffile.TiffWriter(f"{train_path}/{image_name}_h_flip.tif") as tif:
-            tif.write(image_h_flip.numpy(), shape=image.shape)
+            tif.write(image_h_flip.numpy(), shape=image.shape, photometric="separated")
         mask_h_flip.save(f"{train_path}/masks/{mask_name}_h_flip_mask.png")
 
         #random vertical flip
         image_v_flip = TF.vflip(image)
         mask_v_flip = TF.vflip(mask)
-        # print(image_v_flip.numpy().min(), image_v_flip.numpy().max())
+        # print(image_v_flip.numpy().min(), image_v_flip.numpy().max(), image_v_flip.numpy().dtype)
         with tifffile.TiffWriter(f"{train_path}/{image_name}_v_flip.tif") as tif:
-            tif.write(image_v_flip.numpy(), shape=image.shape)
+            tif.write(image_v_flip.numpy(), shape=image.shape, photometric="separated")
         mask_v_flip.save(f"{train_path}/masks/{mask_name}_v_flip_mask.png")
 
 
         # # random rotation
-        image_rotated = TF.rotate(image, angle=25.0)
+        rotated_image = np.zeros_like(image)
+        for channel in range(image.shape[0]):
+            rotated_image[channel, :, :] = rotate(image[channel, :, :], angle=25)
+
         mask_rotated = TF.rotate(mask, angle=25.0)
-        # print(image_rotated.numpy().min(), image_rotated.numpy().max())
+        # print(rotated_image.numpy().min(), rotated_image.numpy().max(), rotated_image.numpy().dtype)
 
         with tifffile.TiffWriter(f"{train_path}/{image_name}_rotated.tif") as tif:
-            tif.write(image_rotated.numpy(), shape=image.shape)
+            tif.write(rotated_image, shape=image.shape, photometric="separated")
         mask_rotated.save(f"{train_path}/masks/{mask_name}_rotated_mask.png")
 
         # print('----------------------------------------')
